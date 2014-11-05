13395a53a45e579f6b65b7c04a5bc917d81af5de
Otter1201 <sticky510134@yahoo.com.tw>
2014-11-05 17:17:28 +0800

    Say Hello to User --fix

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -2,10 +2,10 @@ class SayController < ApplicationController
   def hello
     @date = "#{Time.now.year}/#{Time.now.month}/#{Time.now.day} #{Time.now.hour}:#{Time.now.min}"
     # add line, give val to view
-  end
   def user
   def hi
     @username = get 'say/user'
+  end
   end
   end
 end
